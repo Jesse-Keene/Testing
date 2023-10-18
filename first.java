9a36dd5cd92b2efed62c5c8ab00060f43c85f5c4
andrei <rcarullo741@gmail.com>
2023-10-18 11:40:00 +0800

    jjkagds

--- first.java
+++ first.java
@@ -2,6 +2,6 @@ package Testing.Testing;
 
 public class first{
     public static void main(String [] args){
-        System.out.println("tang ina mon jessse run moto");
+        System.out.println("lunch na");
     }
 }
